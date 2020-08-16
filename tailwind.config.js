fd9a511a547fead88050c77f371ba376534e116f
abdalahshaban <abdalahshaban@yahoo.com>
2020-08-16 17:18:50 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: {
+    // enabled: true,
+    // content: ['./src/**/*.html', './src/**/*.ts'],
+  },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#cc0b0e",
+        secondar: {
+          100: "gray",
+          200: "black"
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
